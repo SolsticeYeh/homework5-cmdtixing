811c65a0aeeabbe30e4a20d610575ec00348b269
生 叶 <yezhou2@outlook.com>
2015-11-15 17:11:58 +0800

    homework5-命令行参数梯形

---
+++ homework5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)//当输入两个参数时
+            {
+                int l = Convert.ToInt32(args[0]);
+                int h = Convert.ToInt32(args[1]);
+                for (int i = 0; i < h; i++)//梯形算法
+                {
+                    for (int j = 0; j <= h - 1 - i; j++)//输入空格
+                        Console.Write(" ");
+                    for (int k = 1; k <= l + 2 * i; k++)//输入星号
+                        Console.Write("*"); Console.WriteLine();
+                }
+            }
+            else//当输入的参数不为两个时
+            {
+                Console.WriteLine("请输入两个命令行参数作为梯形的上底和高");//提示重新输入两个参数
+            }
+        }
+    }
+}
